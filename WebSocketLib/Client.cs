6d34507501e46fbfe11df7e11106ed97403485d7
iedred7584 <git@iedred7584.com>
2020-04-12 14:44:54 +0900

    Update Client

--- WebSocketLib/Client.cs
+++ WebSocketLib/Client.cs
@@ -14,6 +14,7 @@ namespace WebSocketLib
         public Client(int bufferSize)
         {
             _messageBufferSize = bufferSize;
+            _cws = new ClientWebSocket();
         }
 
         /// <summary>
@@ -21,10 +22,10 @@ namespace WebSocketLib
         /// </summary>
         public async void Open(string uri)
         {
+            _cws ??= new ClientWebSocket();
+
             try
             {
-                _cws = new ClientWebSocket();
-
                 if (_cws.State == WebSocketState.Open) return;
                 await _cws.ConnectAsync(new Uri(uri), CancellationToken.None);
                 Opened?.Invoke();
@@ -51,24 +52,28 @@ namespace WebSocketLib
         /// Opened WebSocket Event.
         /// </summary>
         public event OpenedEventHandler Opened;
+
         public delegate void OpenedEventHandler();
 
         /// <summary>
         /// Message Receive Event.
         /// </summary>
         public event ReceiveEventHandler OnMessage;
+
         public delegate void ReceiveEventHandler(string message);
 
         /// <summary>
         /// Closed WebSocket Event.
         /// </summary>
         public event CloseEventHandler Closed;
+
         public delegate void CloseEventHandler();
 
         /// <summary>
         /// Message Receive Event.
         /// </summary>
         public event ErrorEventHandler Error;
+
         public delegate void ErrorEventHandler(Exception exception);
 
         /// <summary>
@@ -78,7 +83,7 @@ namespace WebSocketLib
 
         public void Dispose()
         {
-            if (_cws.State == WebSocketState.Open) Close();
+            if (_cws != null && _cws.State == WebSocketState.Open) Close();
             _cws?.Dispose();
         }
     }
